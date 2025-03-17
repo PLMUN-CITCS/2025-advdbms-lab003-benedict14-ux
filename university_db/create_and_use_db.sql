815a1d9056d3e02ed3377d3eb642061e62dddb81
John Benedict Alvarez <alvarezjohnbenedict_bsit@plmun.edu.ph>
2025-03-17 17:03:44 +0800

    Submitting ADVDBMS Week 02 - Session 01 - Exercise 01

---
+++ university_db/create_and_use_db.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE `UniversityDB`;				-- create database
+USE `UniversityDB`;							-- use the newly created database
+SELECT DATABASE();							-- This will show the currently selected database.
